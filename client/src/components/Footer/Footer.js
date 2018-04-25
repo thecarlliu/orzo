ca8bed3091d1e5ac98e2f7aab319d4f9f07a4c6c
CarL Liu <thecarlliu99@CarLs-MacBook-Air.local>
2018-04-24 19:51:15 -0500

    Created Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,40 +1,33 @@
-//TODO: Update Footer Component
-//Update hrefs and add style
-
 import React from "react"
 import "./Footer.scss"
 
+const artistInfo = {
+    name: "Pasta Orzo",
+    resume: "wkjfwofj",
+    email: "oewifowifn",
+    linkedIn: "oefoif3",
+    medium1: "/medium1",
+    medium2: "/medium2",
+    medium3: "/medium3"
+};
+
 const Footer = () => (
-    <footer className="marketing-site-footer grid-x medium-12">
-        <div className="medium-unstack grid-x medium-12">
-            <div className="cell medium-6">
-                <h4 className="marketing-site-footer-name">Carbos</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita dolorem accusantium architecto id quidem, itaque nesciunt quam ducimus atque.</p>
-            </div>
-            <div className="cell medium-6">
-                <ul className="menu marketing-site-footer-menu-social simple grid-x medium-12">
-                    <li><a href="https://github.com/thecarlliu/carbos"><img src="https://via.placeholder.com/100x100"/></a></li>
-                    <li><a href="https://github.com/thecarlliu/carbos"><img src="https://via.placeholder.com/100x100"/></a></li>
-                    <li><a href="https://github.com/thecarlliu/carbos"><img src="https://via.placeholder.com/100x100"/></a></li>
-                    <li><a href="https://github.com/thecarlliu/carbos"><img src="https://via.placeholder.com/100x100"/></a></li>
-                </ul>
-            </div>
+    <footer className="grid-x medium-12">
+        <div className="cell medium-6">
+            <h5>{artistInfo.name}</h5>
+                <a href={artistInfo.resume}>resume</a><br/>
+                <a href={artistInfo.email}>email</a><br/>
+                <a href={artistInfo.linkedIn}>linkedIn</a>
+
         </div>
-        <div className="marketing-site-footer-bottom grid-x medium-12">
-            <div className="large-unstack align-middle grid-x medium-12">
-                <div className="cell medium-6">
-                    <p>&copy; 2018 Carbos</p>
-                </div>
-                <div className="cell medium-6">
-                    <ul className="menu marketing-site-footer-bottom-links">
-                        <li><a href="/">Back to Top</a></li>
-                        <li><a href="/docs">Docs</a></li>
-                        <li><a href="/templates">Templates</a></li>
-                        <li><a href="/forum">Forum</a></li>
-                        <li><a href="/team">Team</a></li>
-                    </ul>
-                </div>
-            </div>
+        <div className="cell medium-6">
+            <h5>Galleries</h5>
+            <a href={artistInfo.medium1}>medium1</a><br/>
+            <a href={artistInfo.medium2}>medium2</a><br/>
+            <a href={artistInfo.medium3}>medium3</a>
+        </div>
+        <div className="cell medium-2">
+            <p>Copy right artist2018</p>
         </div>
     </footer>
 );
