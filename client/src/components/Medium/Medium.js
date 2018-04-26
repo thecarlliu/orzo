187506f9301b5d0b86e668270442c0c5cc10eaef
CarL Liu <thecarlliu99@er-10-129-40-169.wireless.umn.edu>
2018-04-25 21:20:35 -0500

    Ready for styling

--- client/src/components/Medium/Medium.js
+++ client/src/components/Medium/Medium.js
@@ -3,7 +3,7 @@ import "./Medium.css";
 
 const Medium = (props) => (
     <div className="medium-section cell small-12 medium-12">
-        <h3 className"medium-section-title">{props.name}</h3>
+        <h3 className="medium-section-title">{props.name}</h3>
         <a className="medium-section-link" href={props.name}>
             <img className="medium-section-image" src={props.image} alt={props.name}/>
         </a>
--- models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    General: require("./General"),
-    Support: require("./Support")
-};
