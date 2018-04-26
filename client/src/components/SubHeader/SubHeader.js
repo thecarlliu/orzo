e9839e1a0903342258b65691955075b007dadd6c
CarL Liu <thecarlliu99@er-10-129-40-169.wireless.umn.edu>
2018-04-25 21:23:13 -0500

    Fixed Link

--- client/src/components/SubHeader/SubHeader.js
+++ client/src/components/SubHeader/SubHeader.js
@@ -3,7 +3,7 @@ import "./SubHeader.css";
 
 const SubHeader = (props) => (
     <div className="subheader-links">
-        <h3><a href={props.resumeLink}>resume</a> | <a href={props.email}>email</a> | <a href={props.linkedInLink}>linkedIn</a></h3>
+        <h3><a href={props.resumeLink}>resume</a> | <a href={props.emailLink}>email</a> | <a href={props.linkedInLink}>linkedIn</a></h3>
         <h4><a href={props.medium1}>{props.medium1}</a> | <a href={props.medium2}>{props.medium2}</a> | <a href={props.medium3}>{props.medium3}</a></h4>
     </div>
 );
